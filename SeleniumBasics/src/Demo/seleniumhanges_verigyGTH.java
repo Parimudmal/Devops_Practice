ae813772553d65e44ae94bbf3f10e2de99a0210c
Parimudmal <shreya999@gmail.com>
2019-10-07 12:23:35 -0500

    adding changes to selenium project class

--- SeleniumBasics/src/Demo/seleniumhanges_verigyGTH.java
+++ SeleniumBasics/src/Demo/seleniumhanges_verigyGTH.java
@@ -1,5 +1,9 @@
 package Demo;
 
 public class seleniumhanges_verigyGTH {
-
+	public static void main(string[] args)
+	{
+		System.out.println("selenium class change");
+		System.out.println(" 2nd selenium change");
+	}
 }
