6dd6319941102de15ab61379e999a3354a12f157
Parimudmal <shreya999@gmail.com>
2019-10-07 12:27:03 -0500

    another change in second print

--- SeleniumBasics/src/Demo/seleniumhanges_verigyGTH.java
+++ SeleniumBasics/src/Demo/seleniumhanges_verigyGTH.java
@@ -4,6 +4,6 @@ public class seleniumhanges_verigyGTH {
 	public static void main(string[] args)
 	{
 		System.out.println("selenium class change");
-		System.out.println(" 2nd selenium change");
+		System.out.println(" 2nd ,,,selenium change");
 	}
 }
